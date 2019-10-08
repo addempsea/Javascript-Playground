88be10bb53f879f56aaa99b9959ec7e78607a007
addempsea <babatundeademola4@gmail.com>
2019-10-08 16:15:26 +0100

    dos

---
+++ login.js
@@ -0,0 +1,130 @@
+// var userName = prompt('who\'s there?');
+// userName = userName.toLowerCase();
+// switch (userName) {
+//     case "admin":
+//         alert ('please input your password');
+//         let password = prompt ('password?')
+//         switch (password) {
+//             case "TheMaster":
+//                 alert ('Welcome Admin');
+//                 break;
+//             case null:
+//                 alert ('cancelled')
+//                 break;
+//             default:
+//                 alert ('Wrong password');
+//         }
+//         break;
+//     case null:
+//         alert('cancelled');
+//         break;
+//     default:
+//         alert ('i don\'t know you');
+// }
+
+// var userName = prompt("who's there?");
+// userName = userName.toLowerCase();
+// // console.log(userName);
+// if (userName == "admin") {
+//     let password = prompt("please input your password");
+//     // password = password.toLowerCase();
+//     if (password == "themaster") {
+//         alert("welcome Admin!");
+//     } else if (password == null) {
+//         alert("cancelled");
+//     } else alert("wrong password");
+// } else if (userName == null) {
+//     alert("cancelled");
+// } else alert("i don't know you");
+
+//     function demo (x, y) {
+//         let kd = x ** y;
+//         return kd
+//     }
+// var k = prompt('enter a value');
+
+// var i = prompt('enter a value');
+
+// alert (demo(k,i));
+
+// var info = {
+//     // name: 'Babatunde Ademola',
+//     // course: 'Mechanical Engineer',
+//     // address: 'ibadan, Oyo state',
+//     age: 17
+// };
+
+// for (let age in info) {
+//     let  kd = (info[age]);
+//     alert ('i am ' + kd);
+// }
+
+// for(key in arr) {
+//     if(data.hasOwnProperty(key)) {
+//         var value = data[key];
+//         //do something with value;
+//     }
+// }
+// console.log (Object.values(demo.age));
+
+// alert(demo.age );
+
+// var demo = { name: "demo", age: 15 };
+// var semo = { name: "semo", age: 25 };
+// var vemo = { name: "vemo", age: 35 };
+// var temo = { name: "temo", age: 45 };
+
+// var arr = [demo.age, semo.age, vemo.age, temo.age];
+
+// var arry = [demol.age, semol.age, vemol.age, temol.age];
+
+
+
+// getTotal = function (x, y) {
+//   var newSum = x + y;
+
+//   return newSum;
+// };
+
+// var sum = arry.reduce(getTotal);
+
+// function getMean() {
+//   let mean = sum / parseInt(arry.length);
+//   return mean.toFixed();
+// }
+
+// console.log(getMean(arry));
+
+// console.log(arry.sort(function(a, b){return a-b}));
+// console.log(rry.sort());
+// console.log(arr.sort());
+// // document.write(arr.sort());
+// console.log(arr[1].name);
+
+// function kd () {
+  // console.log(arr);
+// }
+
+var demol = { name: "demo", age: 5 };
+var semol = { name: "zemo", age: 25 };
+var vemol = { name: "kemo", age: 15 };
+var temol = { name: "cemo", age: 50 };
+
+var arr = [temol, demol, vemol, semol];
+var rry = [demol.name, semol.name, vemol.name, temol.name];
+console.log(temol);
+// var ii = ['t', 1];
+// var yy = ['s', 2];
+// var oo = ['v', 6];
+// var uu = [ii, yy, oo] 
+
+// console.log(arr.sort());
+
+// console.log(kd(arr));
+
+// console.log(arr.sort(function(x, y){
+
+//   if (x < y) {return -1;}
+//   if (x > y) {return 1;}
+//   return 0;
+// }));
